4067dae24e10394d56ac2e13a65abf7f3799c0b1
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-28 20:07:38 +0700

    Update MainActivity.kt
    СДЕЛАЛ ГЛАВНЫЙ ЭКРАН И ПОЛОВИНУ ИГРОВОГО ЭКРАНА

--- Clicker/app/src/main/java/com/example/clicker/MainActivity.kt
+++ Clicker/app/src/main/java/com/example/clicker/MainActivity.kt
@@ -70,5 +70,58 @@ fun MainCreen(onNavigateToGame: () -> Unit, onNavigateToAchievements: () -> Unit
             .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+        ) {
+        Text(
+            text = "Cool Clicker",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 48.dp)
+        )
+        Button(
+            onClick = onNavigateToGame,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(bottom = 16.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text(text = "In Game", fontSize = 18.sp)
+        }
+        Button (
+            onClick = onNavigateToAchievements,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(bottom = 16.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6200EE))
+        ) {
+            Text(text = "Achievements", fontSize = 18.sp)
+        }
+    }
+}
+
+@Composable
+fun GameScreen(clicks: Int, balance: Int, onClick: () -> Unit, onBack: () -> Unit) {
+    Column(
+    modifier = Modifier
+        .fillMaxSize()
+        .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+        TextButton(onClick = onBack) {
+         Text(text = "Back", fontSize = 16.sp)
+        }
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5))
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
+            }
+        }
+    }
 }
